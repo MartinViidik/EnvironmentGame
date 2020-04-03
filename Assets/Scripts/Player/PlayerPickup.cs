0c03311c3d9e0a86dd26cf64ff5612ab3766d583
Martin Viidik <martinviidik@gmail.com>
2020-04-03 21:10:12 +0300

    Player pickup stuff logic

---
+++ Assets/Scripts/Player/PlayerPickup.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    public GameObject pickupUI;
+    private GameObject resourceRef;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Resource"))
+        {
+            resourceRef = col.gameObject;
+            SetPickupUI(true);        
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Resource"))
+        {
+            resourceRef = null;
+            SetPickupUI(false);
+        }
+    }
+
+    public void PickupItem()
+    {
+        resourceRef.SetActive(false);
+        resourceRef = null;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && resourceRef != null)
+        {
+            SetRadialFill(true);
+            Debug.Log("test");
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            SetRadialFill(false);
+        }
+    }
+
+    void SetPickupUI(bool state)
+    {
+        pickupUI.SetActive(state);
+    }
+
+    public void SetRadialFill(bool state)
+    {
+        pickupUI.GetComponent<RadialPickupUI>().SetFilling(state);
+    }
+}
---
+++ Assets/Scripts/UI/RadialPickupUI.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialPickupUI : MonoBehaviour
+{
+    public Image radialFill;
+    public GameObject playerRef;
+    public Camera cam;
+    bool isFilling;
+
+    private void OnEnable()
+    {
+        InvokeRepeating("PositionObject", 0.0f, 0.01f);
+    }
+
+    void Update()
+    {
+        if (isFilling)
+        {
+            radialFill.fillAmount += Time.deltaTime;
+            CheckIfFull();
+        } else {
+            radialFill.fillAmount -= Time.deltaTime;
+        }
+    }
+
+    Vector2 AbovePlayer()
+    {
+        float x = playerRef.transform.position.x;
+        float y = playerRef.transform.position.y + 2;
+        return new Vector2(x, y);
+    }
+
+    public void SetFilling(bool state)
+    {
+        isFilling = state;
+    }
+    void CheckIfFull()
+    {
+        if(radialFill.fillAmount == 1)
+        {
+            isFilling = false;
+            playerRef.GetComponent<PlayerPickup>().PickupItem();
+        }
+    }
+
+    void PositionObject()
+    {
+        transform.position = cam.WorldToScreenPoint(AbovePlayer());
+    }
+}

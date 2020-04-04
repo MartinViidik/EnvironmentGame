f974af98cf524e07055c159a3bd053944138df68
Martin Viidik <martinviidik@gmail.com>
2020-04-04 19:34:39 +0300

    Added pause menu

---
+++ Assets/Scripts/Game/PauseController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale == 1.0)
+            {
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0;
+            } else
+            {
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+}

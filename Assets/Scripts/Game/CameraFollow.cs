091ad585023ba81514f3d7e3c0f2d384c2d78294
Martin Viidik <martinviidik@gmail.com>
2020-04-04 15:58:34 +0300

    Camera follow

---
+++ Assets/Scripts/Game/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float interpVelocity;
+    public float minDistance;
+    public float followDistance;
+    public GameObject target;
+
+    public Vector3 offset;
+    Vector3 targetPos;
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
+
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+
+            interpVelocity = targetDirection.magnitude * 5f;
+
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime * 5);
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+        }
+    }
+}

de05fe2d25bce44c704ebe8896e20d3f2cf4d0a5
NeguliSimanis <simanism@inbox.lv>
2020-04-04 03:52:05 +0300

    added geyser that throws player around

---
+++ Assets/Scripts/Hazards/Geyser.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geyser : MonoBehaviour
+{
+    private float inactiveGeyserMinDuration = 2f;
+    private float inactiveGeyserMaxDuration = 4f;
+    private float geyserWarningDuration = 1f;
+    private float activeGeyserDuration = 3f;
+
+    private bool isGeyserActive = false;
+    private bool hurtPlayerDuringActivity = false;
+
+    private ParticleSystem geyserParticleSystem;
+    private int geyserEmissionRate = 90;
+
+    [SerializeField]
+    private Animator geyserAnimator;
+
+    private PlayerMovement playerMovement;
+
+    private float collisionUpThrow = 180;
+    private float collisionDefaultUpThrow = 180;
+    private float geyserUpThrowStartPosition;
+    private bool geyserThrowUpLocationSet = false;
+
+    private void Start()
+    {
+        geyserParticleSystem = GetComponent<ParticleSystem>();
+        geyserParticleSystem.emissionRate = 0;
+        StartCoroutine(ActivateGeyserAfterDuration());
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    private IEnumerator ActivateGeyserAfterDuration()
+    {
+        
+        float duration = Random.Range(inactiveGeyserMinDuration, inactiveGeyserMaxDuration);
+        yield return new WaitForSeconds(duration);
+
+        geyserAnimator.SetBool("isWarning", true);
+        yield return new WaitForSeconds(geyserWarningDuration);
+        geyserAnimator.SetBool("isWarning", false);
+
+        isGeyserActive = true;
+        geyserParticleSystem.emissionRate = geyserEmissionRate;
+        StartCoroutine(EndGeyserActivityAfterDuration());
+    }
+
+    private IEnumerator EndGeyserActivityAfterDuration()
+    {
+        yield return new WaitForSeconds(activeGeyserDuration);
+        isGeyserActive = false;
+        hurtPlayerDuringActivity = false;
+        geyserParticleSystem.emissionRate = 0;
+        StartCoroutine(ActivateGeyserAfterDuration());
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && isGeyserActive)
+        {
+            HurtPlayer();
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")// && !hurtPlayerDuringActivity)
+        {
+            if (isGeyserActive)
+            {
+
+                ThrowPlayerUpwards(collision);
+                HurtPlayer();
+            }
+            else
+            {
+                geyserThrowUpLocationSet = false;
+                collisionUpThrow = collisionDefaultUpThrow;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            geyserThrowUpLocationSet = false;
+            collisionUpThrow = collisionDefaultUpThrow;
+        }
+    }
+
+    //private void LetPlayerFall(Collider2D collision)
+    //{
+    //    collisionUpThrow = collisionDefaultUpThrow;
+    //    if (geyserThrowUpLocationSet)
+    //    {
+    //        geyserThrowUpLocationSet = false;
+            
+    //    }
+    //}
+
+    private void ThrowPlayerUpwards(Collider2D collision)
+    {
+        if (!geyserThrowUpLocationSet)
+        {
+            geyserUpThrowStartPosition = transform.position.y + 0.5f;
+            playerMovement.FallDown(geyserUpThrowStartPosition);
+            playerMovement.FlyBackwards();
+            geyserThrowUpLocationSet = true;
+        }
+        if (collisionUpThrow > 2)
+        {
+            collisionUpThrow += 2.5f - 2f*(playerMovement.gameObject.transform.position.y - geyserUpThrowStartPosition);
+        }
+        collision.attachedRigidbody.AddForce(Vector2.up * collisionUpThrow);
+    }
+
+    private void HurtPlayer()
+    {
+        hurtPlayerDuringActivity = true;
+        playerMovement.ThrowPlayerBack();
+    }
+
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Attributes")]
@@ -7,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Animator anim;
+    SpriteRenderer playerSprite;
 
     float horizontal;
     float vertical;
@@ -14,12 +16,20 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movementDirection;
     float lastDirectionHorizontal = 0;
     float lastDirectionVertical = 0;
+
+    bool checkForGround = false;
+    bool isFlyingBackwards = false;
+    Direction flyingDirection = Direction.East;
+
+    float groundCoordinate;
+
     void Awake()
     {
         if (rb == null)
         {
             rb = GetComponent<Rigidbody2D>();
         }
+        playerSprite = anim.gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -41,12 +51,32 @@ public class PlayerMovement : MonoBehaviour
             Animate(lastDirectionHorizontal, lastDirectionVertical, 0);
         }
         float lastDir = transform.eulerAngles.y;
+
+        if (rb.IsSleeping())
+        {
+            rb.WakeUp();
+        }
+        if (checkForGround && transform.position.y < groundCoordinate)
+        {
+            rb.gravityScale = 0;
+        }
+        if (isFlyingBackwards)
+        {
+            if (flyingDirection == Direction.East)
+                rb.AddForce(Vector2.right * 110);
+            else
+                rb.AddForce(Vector2.left * 110);
+        }
     }
 
     void GetInput()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        // don't allow up-down movment when flying in geyser vapors
+        if (rb.gravityScale < 1)
+            vertical = Input.GetAxisRaw("Vertical");
+        else
+            vertical = 0;
     }
 
     void Animate(float horizontal, float vertical, float speed)
@@ -56,4 +86,44 @@ public class PlayerMovement : MonoBehaviour
         anim.SetFloat("Speed", speed);
     }
 
+    public void ThrowPlayerBack()
+    {
+        //Debug.Log("I'M HURT!");
+    }
+
+    float fallSpeed = 10f;
+    float defaultFallSpeed = 10f;
+    public void FallDown(float groundPositionY)
+    {
+        StartCoroutine(EnableGravityForDuration(4.5f, groundPositionY));
+    }
+
+
+    public void FlyBackwards()
+    {
+        StartCoroutine(EnableFlyingBackwardsForDuration(0.7f));
+        if (!playerSprite.flipX)
+            flyingDirection = Direction.East;
+        else
+            flyingDirection = Direction.West;
+    }
+
+    private IEnumerator EnableFlyingBackwardsForDuration(float duration)
+    {
+        isFlyingBackwards = true;
+        yield return new WaitForSeconds(duration);
+        isFlyingBackwards = false;
+    }
+
+    private IEnumerator EnableGravityForDuration(float duration, float lowestAllowedCoordinate)
+    {
+        rb.gravityScale = 0;
+        yield return new WaitForSeconds(0.8f);
+        groundCoordinate = lowestAllowedCoordinate;
+        checkForGround = true;
+        rb.gravityScale = 21f;
+        yield return new WaitForSeconds(duration);
+        checkForGround = false;
+        rb.gravityScale = 0;
+    }
 }
